145f64b0a224f25c0a3c0dd46ac187c759565471
sakthigowri01 <35915299+sakthigowri01@users.noreply.github.com>
2018-02-12 13:44:53 +0530

    gj.c

---
+++ gj.c
@@ -0,0 +1,21 @@
+import java.util.*;
+public class sum_num_odd
+{
+    
+    {
+      
+        for(int i=1;i<=45;i++)
+        {
+            if(i>=1&&i<=15)
+            
+            
+            }
+            if(i>=15&&i<=45)
+            {
+                if(i%2==1)
+                {
+                    sum=sum+i;
+                }
+                
+            }
+        }
