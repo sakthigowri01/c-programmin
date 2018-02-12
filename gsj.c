71cc09c8ddf8ef4dd4876ca52a08ce3d15ad9630
sakthigowri01 <35915299+sakthigowri01@users.noreply.github.com>
2018-02-12 13:49:18 +0530

    gsj.c

---
+++ gsj.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+void main()
+{
+	char a[50];
+	int i,n,count=0;
+
+	printf("THE LENGTH OF STRING IS %d",n);
+	for(i=0;i<n;i++)
+	{
+		if(isalpha(a[i]))
+		
+
+	printf("\nTHE NO.OF.CHARCTERS IN THE GIVEN STRING IS %d",count);
+}
