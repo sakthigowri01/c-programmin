c68683fb9deb956c6383e794aa437ae166440728
sakthigowri01 <35915299+sakthigowri01@users.noreply.github.com>
2018-02-12 14:03:00 +0530

    gv.c

---
+++ gv.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+void main()
+{
+   char str[100];
+   int i = 0, l = 0, f = 1;
+   clrscr();
+   puts("Enter any string\n");
+   gets(str);
+   for(i = 0; str[i] !='\0'; i++)
+  
+   }
+   {
+   printf("The number of characters in the string are %d\n", l);
+   for(i = 0; i <= l-1; i++)
+   {
+     
+   printf("The number of words in the string are %d", f);
+   getch();
+}
