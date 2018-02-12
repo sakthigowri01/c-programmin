85f13bda0ac2f4baf795e3b65b535afd1a817981
sakthigowri01 <35915299+sakthigowri01@users.noreply.github.com>
2018-02-12 14:13:00 +0530

    stg.c

---
+++ stg.c
@@ -0,0 +1,18 @@
+int main()
+{
+      double firstNumber, secondNumber, temporaryVariable;
+
+      printf("Enter first number: ");
+      scanf("%lf", &firstNumber);
+
+      printf("Enter second number: ");
+      scanf("%lf",&secondNumber);
+
+      // Value of firstNumber is assigned to temporaryVariable
+      temporaryVariable = firstNumbe
+
+      printf("\nAfter swapping, firstNumber = %.2lf\n", firstNumber);
+      printf("After swapping, secondNumber = %.2lf", secondNumber);
+
+      return 0;
+}
