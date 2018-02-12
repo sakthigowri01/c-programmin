d7ee547f3884cd7396b1d255953934cd85cd6493
sakthigowri01 <35915299+sakthigowri01@users.noreply.github.com>
2018-02-12 13:57:56 +0530

    prani.c

---
+++ prani.c
@@ -0,0 +1,10 @@
+{ int a=0;
+    char ch[]= new char[s.length()];
+    for(int i = 0; i < s.length(); i++) 
+
+    {  ch[i]= s.charAt(i);
+        if( ch[i]==' ' )
+     
+            }   
+    return a;
+}
